5438faaa371a3f7971b7a88e1604b7cdda6f156d
Mbabazi-Angel <144022384+Mbabazi-Angel@users.noreply.github.com>
2023-10-19 11:10:36 +0300

    Add files via upload

---
+++ Functions/python_test_one/question_three.py
@@ -0,0 +1,21 @@
+marks=[78,83,90,88,75]
+sum=(78+83+90+88+75)
+print(sum)
+   
+
+
+# ii)
+print(marks[0])
+print(marks[4])
+
+
+
+
+#iii
+marks.append(96)
+print(marks)
+
+
+#iv)
+marks=[78,83,90,88,75]
+marks.append(87)
